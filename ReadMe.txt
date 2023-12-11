97d1354883479fc29d24e1ade058e5b3843f9824
Olivier2Carglass <104084572+oliver2Carglass@users.noreply.github.com>
2023-12-11 10:50:56 -0400

    last commit
    Only adding commentary and documentation to have a more readable code

--- ReadMe.txt
+++ ReadMe.txt
@@ -3,3 +3,5 @@ This is a Shopping web site for Harry Potter fans where you can buy film and boo
 Package.json contain the price the name and an image of the current article
 
 stock.json contain the database of the stock depending of if a book or movie is in stock
+
+My website work only when i am using my liveserver extension web because of CORS authorization
--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
+
+// Create a view app
 const app = Vue.createApp({
     data() {
         return {
-            currentSection: 'book',
-            HPCollection: [], // Ajoutez une propriété pour stocker les données du package.json
-            stockData: [], // Ajoutez une propriété pour stocker les données du stock.json
-            currentBookIndex: 0, // Ajoutez une propriété pour suivre l'index actuel du livre
-            total: 0,
-            stock: false,
-            email: '', // Ajoutez une propriété pour stocker l'e-mail
+            currentSection: 'book', //section indicator
+            HPCollection: [], // data of package.json
+            stockData: [], // data of stock.json
+            currentBookIndex: 0, // variable of the index of the Harry Potter crrousel
+            total: 0, // total price (sum)
+            stock: false, //variable of if the current item is in stock
+            //form vairables
+            email: '', 
             feedback: ''
         };
     },
+
     mounted() {
-        // Fetch des fichiers JSON en parallèle
+        // Fetching json files
         Promise.all([
             fetch('../package.json').then(response => response.json()),
             fetch('../stock.json').then(response => response.json())
         ])
         .then(data => {
-            // Stockage des données dans les propriétés correspondantes
+            // Stocking data in variables
             this.HPCollection = data[0].harryPotterCollection;
             this.stockData = data[1];
 
-            // Mise à jour de la variable stock avec la valeur de l'élément actuel de book ou movie
+            // Uptdating stock for the first time
             this.updateStock();
         })
         .catch(error => console.error('Erreur de chargement des fichiers JSON:', error));
     },
+
+    //methods
     methods: {
+        //method to swip section and updating the stock corresponding to the item
         swipSection(section) {
             this.currentSection = section;
-            // Mise à jour de la variable stock lorsque la section change
             this.updateStock();
         },
+
+        //carousel previus and next item gestion
         nextBook() {
             if (this.currentBookIndex < this.HPCollection.length - 1) {
                 this.currentBookIndex++;
                 this.updateStock()
             }
-            // Mise à jour de la variable stock lorsque le livre change
             this.updateStock();
         },
         prevBook() {
@@ -46,30 +53,33 @@ const app = Vue.createApp({
                 this.currentBookIndex--;
                 this.updateStock()
             }
-            // Mise à jour de la variable stock lorsque le livre change
             this.updateStock();
         },
+
+        //addValue is a method that add the current object price to the "total" variable if the stock is available
         addValue() {
+            if(this.stock){
             this.total += this.HPCollection[this.currentBookIndex][this.currentSection].price;
-            console.log(this.currentSection, this.total);
+            console.log(this.currentSection, this.total);}
         },
+
+        //updtaeStock is a method that look into the json file to check if the sotck is up or not, then apply difrent css classes by using the stock variable
         updateStock() {
             const currentBook = this.HPCollection[this.currentBookIndex];
             
-            // Vérifie si l'élément actuel existe dans stockData
+            // Cheking into json
             const stockElement = this.stockData.stock.find(element => element.bookName === currentBook.bookName);
             
-            // Met à jour la variable stock en fonction de la valeur de l'élément dans stockData
+            // Updating variable
             this.stock = stockElement ? stockElement[this.currentSection] : false;
     
-            // Correction pour mettre à jour correctement la section "movie"
             if (this.currentSection === 'movie') {
                 this.stock = stockElement ? stockElement.movie : false;
             }
         },
         
         submitFeedback() {
-            // Ici, tu peux envoyer l'e-mail et le feedback à un serveur, enregistrer dans une base de données, etc.
+            // getting the view form variable and printing it into console
             console.log('E-mail:', this.email);
             console.log('Feedback:', this.feedback);
 
